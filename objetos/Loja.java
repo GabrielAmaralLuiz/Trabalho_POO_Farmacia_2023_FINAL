3f8624671a4afcfc923d73ec9e454738e6c4619b
Gabriel Amaral Luiz <104163991+GabrielAmaralLuiz@users.noreply.github.com>
2023-05-22 20:14:52 -0300

    Add files via upload

---
+++ objetos/Loja.java
@@ -0,0 +1,67 @@
+package objetos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Loja {
+private int codigo;
+private String nome;
+
+ArrayList<Produto> produtos;
+
+
+public Loja(int codigo, ArrayList<Produto> produtos,String nome) {
+	this.codigo = codigo;
+	
+	this.produtos = produtos;
+	this.nome = nome;
+}
+
+public Loja() {
+}
+	
+
+void mostrarProdutos(){
+	Iterator<Produto> itr = produtos.iterator();
+while (itr.hasNext()) {
+	Produto produto = (Produto) itr.next();
+	System.out.println("----Produtos----");
+	produto.mostrarDados();
+}
+	
+}
+
+public int getCodigo() {
+	return codigo;
+}
+
+public void setCodigo(int codigo) {
+	this.codigo = codigo;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public ArrayList<Produto> getProdutos() {
+	return produtos;
+}
+
+public void setProdutos(ArrayList<Produto> produtos) {
+	this.produtos = produtos;
+}
+
+public void mostrarDados() {
+System.out.println("Nome: "+nome);
+System.out.println("Codigo: "+codigo);
+System.out.println();
+
+}
+
+
+	
+}
